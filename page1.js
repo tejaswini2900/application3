9f607d684bc791bb35aa3f1cf7b3c5fc524a3615
tejaswini <tejaswinipatil2900@gmail.com>
2022-06-20 17:04:32 +0530

    oddNumber function  added

--- page1.js
+++ page1.js
@@ -9,3 +9,13 @@ function evenNumber(n){
 }
 evenNumber(10);
 evenNumber(11);
+
+function oddNumber(n){
+    if(n%2 != 0){
+        console.log(n+"= is odd number")
+    }else{
+        console.log(n+"= is even number")
+    }
+}
+oddNumber(550);
+oddNumber(11);
