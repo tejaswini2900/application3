a6ec017444a454d09c46da927bb8fadb9ab706fa
tejaswini <tejaswinipatil2900@gmail.com>
2022-06-20 16:23:07 +0530

    evenNumber function  added

--- page1.js
+++ page1.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+function evenNumber(n){
+    if(n%2 == 0){
+        console.log(n+"= is even number")
+    }else{
+        console.log(n+"= is odd number")
+    }
+}
+evenNumber(10);
+evenNumber(11);
