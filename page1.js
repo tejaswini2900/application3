7741af28f0b62c890dfe795800129354ca0e231b
tejaswini <tejaswinipatil2900@gmail.com>
2022-06-20 17:52:36 +0530

    changes in master

--- page1.js
+++ page1.js
@@ -1,13 +1,10 @@
 console.log("hello")
-<<<<<<< HEAD
 
 function addNum(n1,n2){
     console.log(`${n1}+${n2} = ${n1+n2}`)
 }
 addNum(10,20);
-=======
 function sunNum(n1,n2){
     console.log(`${n1}-${n2}= ${n1-n2}`)
 }
 sunNum(30,15);
->>>>>>> sub
