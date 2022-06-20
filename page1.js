1306096819e0f17a20c1205e7affb33c9e8f2210
tejaswini <tejaswinipatil2900@gmail.com>
2022-06-20 17:50:09 +0530

    mulNum function added

--- page1.js
+++ page1.js
@@ -1,13 +1,15 @@
 console.log("hello")
-<<<<<<< HEAD
 
 function addNum(n1,n2){
     console.log(`${n1}+${n2} = ${n1+n2}`)
 }
 addNum(10,20);
-=======
 function sunNum(n1,n2){
     console.log(`${n1}-${n2}= ${n1-n2}`)
 }
 sunNum(30,15);
->>>>>>> sub
+
+function mulNum(n1,n2){
+    console.log(`${n1}*${n2}= ${n1*n2}`)
+}
+mulNum(5,3);
